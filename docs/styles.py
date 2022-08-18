d32baa6d9aeb2de9c4e30a6e689bfdb9f6367a11
Alejandro Piad <apiad@apiad.net>
2022-08-17 23:30:06 -0400

    Bump version

--- docs/styles.py
+++ docs/styles.py
@@ -2,6 +2,6 @@ from violetear import StyleSheet, Style, Color
 
 sheet = StyleSheet(normalize=True, base=Style().color(Color.gray(0.3)))
 
-sheet.select('body').width(0.8, max=768).margin("auto", top=50)
+sheet.select("body").width(0.8, max=768).margin("auto", top=50)
 
 sheet.render("styles.css")
